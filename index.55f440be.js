8715fe4ed93adc926dbdf7c7a58ba077b083051f
FergusonJoan <97786632+FergusonJoan@users.noreply.github.com>
2023-02-26 14:27:48 +0000

    Deploying to gh-pages from @ FergusonJoan/goit-js-hw-10@b208c03f8eb9120fe2234b7b1a0c21903ea1da7f 🚀

---
+++ index.55f440be.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){e.preventDefault();if(!event.currentTarget.value.trim())return}));
+//# sourceMappingURL=index.55f440be.js.map
---
+++ index.7140565c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){if(e.preventDefault(),!event.currentTarget.value.trim())return}))}();
+//# sourceMappingURL=index.7140565c.js.map
