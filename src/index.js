755a39a2677cb40ef5860638e7aff7fb9ab07736
Joan <prosandcons500@gmail.com>
2023-02-28 10:25:38 +0200

    add notiflix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -7,15 +9,29 @@ const inputCountry = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
 
-inputCountry.addEventListener('input', onSearchCountries);
+inputCountry.addEventListener(
+  'input',
+  debounce(onSearchCountries, DEBOUNCE_DELAY)
+);
+
+const clearItem = item => (item.innerHTML = '');
 
 function onSearchCountries(e) {
   e.preventDefault();
 
-  const value = event.currentTarget.value.trim();
+  const value = e.target.value.trim();
   if (!value) {
-    // clearItem(listEl);
-    // clearItem(infoEl);
+    clearItem(listEl);
+    clearItem(infoEl);
     return;
   }
+
+  fetchCountries(value).then(countries => {
+    console.log(countries);
+    if (countries.length > 10) {
+      clearItem(listEl);
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    return;
+  });
 }
